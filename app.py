18d5bd84fa7717c7e12f25b4ccecaf5bd9b9893b
jingane <jingane.2024k@gmail.com>
2024-06-26 23:07:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 
-def get_recipes(query):
-    url = f"https://m.10000recipe.com/recipe/list.html?q={query}"
+def get_recipes(query1, query2):
+    url = f"https://m.10000recipe.com/recipe/list.html?q={query1}+{query2}"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -18,13 +19,20 @@ def get_recipes(query):
     return recipes
 
 def main():
-    query = "김치"  # 검색할 키워드 입력
-    recipes = get_recipes(query)
+    st.title('음식재료 기반 요리 레시피 검색')
 
-    for recipe in recipes:
-        print(recipe['title'])
-        print(recipe['link'])
-        print()
+    ingredient1 = st.text_input('음식재료 1')
+    ingredient2 = st.text_input('음식재료 2')
+
+    if st.button('레시피 검색'):
+        recipes = get_recipes(ingredient1, ingredient2)
+
+        if recipes:
+            st.header('검색 결과')
+            for recipe in recipes:
+                st.markdown(f"### [{recipe['title']}]({recipe['link']})")
+        else:
+            st.write('검색 결과가 없습니다.')
 
 if __name__ == "__main__":
     main()
